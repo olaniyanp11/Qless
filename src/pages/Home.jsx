60278a8fb30331e3d42fbf1fba88c46e7142e8e3
olaniyanpelumi <olaniyanp11@gmail.com>
2024-08-15 15:07:04 +0100

    update files

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Nav } from '../components/nav'
+import {Nav} from "../components/Nav"
 import Hero from '../assets/queue2.jpg'
 import { Link } from 'react-router-dom'
 import { BiReceipt } from 'react-icons/bi'
